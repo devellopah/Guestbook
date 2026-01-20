99848d924c53deedc5de80bfafb1bc3a3297c452
Islam Ibakaev <islamibakaev@gmail.com>
2026-01-20 03:01:47 +0300

    added templates(views) for pages, transformed initial pages into models

--- views/incs/header.tpl.php
+++ views/incs/header.tpl.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GuestBook :: Home</title>
+    <title>GuestBook :: <?= $title ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -17,6 +17,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                
                 <li class="nav-item">
                     <a class="nav-link" href="register.php">Register</a>
                 </li>
